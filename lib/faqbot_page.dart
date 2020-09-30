c481da495dc8ddcdcba47ba744c8e671df22be46
Ronald Vega <ronald.vega48@gmail.com>
2020-09-29 22:35:54 -0600

    FAQ message model created

--- lib/faqbot_page.dart
+++ lib/faqbot_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class FaqBotPage extends StatelessWidget {
+class FaqBotPage extends StatefulWidget {
+  @override
+  _FaqBotPageState createState() => _FaqBotPageState();
+}
+
+class _FaqBotPageState extends State<FaqBotPage> {
+  final TextEditingController _messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container();
---
+++ lib/models/faqmessage_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FAQMessage {
+  String message;
+  String sender;
+  bool type;
+
+  FAQMessage({@required this.message, @required this.sender, this.type});
+}
