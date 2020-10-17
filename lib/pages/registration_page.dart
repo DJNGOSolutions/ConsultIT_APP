340cb292a15b2bd3b5827463b1aba7e8d633b2ba
Ronald Vega <ronald.vega48@gmail.com>
2020-10-16 21:42:49 -0600

    Creating registration page

---
+++ lib/pages/registration_page.dart
@@ -0,0 +1,206 @@
+import 'package:consult_it_app/utils/neumorphismWidgets/bottom_left_clipper.dart';
+import 'package:consult_it_app/utils/neumorphismWidgets/bottom_left_clipper_bottom.dart';
+import 'package:consult_it_app/utils/neumorphismWidgets/clip_shadow_path.dart';
+import 'package:consult_it_app/utils/neumorphismWidgets/top_right_clipper.dart';
+import 'package:consult_it_app/utils/neumorphismWidgets/top_right_clipper_bottom.dart';
+import 'package:consult_it_app/utils/router.dart';
+import 'package:consult_it_app/utils/styles.dart';
+import 'package:consult_it_app/utils/widgets_lib.dart';
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class RegistrationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    final boxShadow = BoxShadow(
+      color: Colors.grey,
+      offset: Offset(-5, 3),
+      blurRadius: 5,
+      spreadRadius: 10,
+    );
+
+    // Neumorphic colored container with 99% app width
+    final widthNeuContainer = Container(
+      width: width * 0.99,
+      color: kNeumorphicColor,
+    );
+
+    // Neumorphic colored container with 99% app height
+    final heightNeuContainer = Container(
+      height: height * 0.99,
+      color: kNeumorphicColor,
+    );
+
+    return Material(
+      child: Stack(
+        children: <Widget>[
+          Align(
+            child: ClipShadowPath(
+              shadow: boxShadow,
+              clipper: TopRightNeuClipperBtm(),
+              child: widthNeuContainer,
+            ),
+          ),
+          Align(
+            alignment: Alignment(30, -1),
+            child: ClipShadowPath(
+              shadow: boxShadow,
+              clipper: TopRightNeuClipper(),
+              child: widthNeuContainer,
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 30.5),
+            child: ClipShadowPath(
+              shadow: boxShadow,
+              clipper: BottomLeftNeuClipperBtm(),
+              child: heightNeuContainer,
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 9),
+            child: SizedBox(
+              width: width,
+              height: height * 0.99,
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: kHomeImage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 80.5),
+            child: ClipShadowPath(
+              shadow: boxShadow,
+              clipper: BottomLeftNeuClipper(),
+              child: heightNeuContainer,
+            ),
+          ),
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 120, bottom: 60),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10),
+                    shadowColor: kNeumorphicColor.withOpacity(0.5),
+                    elevation: 12.0,
+                    color: Colors.transparent,
+                    child: Text(
+                      'Registrate',
+                      style: TextStyle(
+                          fontSize: 58,
+                          fontFamily: Fonts.secondaryFont,
+                          fontWeight: FontWeight.bold,
+                          color: MyColors.mainColor),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 30),
+                  child:
+                      inputField('Ingrese su usuario', 'usuario', Icons.person),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30),
+                  child: inputField(
+                      'Ingrese su contraseña', 'contraseña', Icons.lock),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 80.0, left: 20, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                          onTap: () => Navigator.pushNamed(context, HOME_ROUTE),
+                          child: Container(
+                            constraints:
+                                BoxConstraints(minWidth: 100, maxWidth: 150),
+                            height: 45,
+                            padding: EdgeInsets.symmetric(horizontal: 25),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Color.fromRGBO(3, 90, 166, 1),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color:
+                                          MyColors.mainColor.withOpacity(0.4),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Center(
+                              child: FittedBox(
+                                child: Text(
+                                  'Ingresar'.toUpperCase(),
+                                  style: TextStyle(
+                                      fontFamily: Fonts.secondaryFont,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => Navigator.pushNamed(context, HOME_ROUTE),
+                          child: Container(
+                            constraints:
+                                BoxConstraints(minWidth: 100, maxWidth: 150),
+                            height: 45,
+                            padding: EdgeInsets.symmetric(horizontal: 25),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Color.fromRGBO(64, 186, 213, 1),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: MyColors.secondaryColor
+                                          .withOpacity(0.4),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Center(
+                              child: FittedBox(
+                                child: Text(
+                                  'Registrarse'.toUpperCase(),
+                                  style: TextStyle(
+                                      fontFamily: Fonts.secondaryFont,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+final kNeumorphicColor = Color.fromRGBO(235, 228, 229, 1); // rgb(235, 228, 229)
+final kHomeImage =
+    'https://user-images.githubusercontent.com/31005114/78748465-b5327d00-799e-11ea-9f40-38d322a9531a.png';
--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,10 +1,12 @@
 import 'package:consult_it_app/pages/faqbot_page.dart';
 import 'package:consult_it_app/pages/home_page.dart';
 import 'package:consult_it_app/pages/login_page.dart';
+import 'package:consult_it_app/pages/registration_page.dart';
 import 'package:flutter/material.dart';
 
 const String FAQ_BOT_ROUTE = 'FAQ_BOT_ROUTE';
 const String HOME_ROUTE = 'HOME_ROUTE';
+const String REGISTRATION_ROUTE = 'REGISTRATION_ROUTE';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
@@ -15,6 +17,9 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       break;
     case HOME_ROUTE:
       return MaterialPageRoute(builder: (context) => HomePage());
+      break;
+    case REGISTRATION_ROUTE:
+      return MaterialPageRoute(builder: (context) => RegistrationPage());
       break;
     default:
       return MaterialPageRoute(builder: (context) => LoginPage());
