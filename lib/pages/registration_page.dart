17b7f3d75c34b0433cbbbc6721823e7ab4dd5b79
Ronald Vega <ronald.vega48@gmail.com>
2020-10-22 19:55:05 -0600

    Adding tab for professional account

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -61,17 +61,33 @@ class _RegistrationPageState extends State<RegistrationPage>
           Container(
             padding: EdgeInsets.only(bottom: 10),
             width: MediaQuery.of(context).size.width,
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Text(
-                'Bienvenido a Consult IT',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: Fonts.secondaryFont,
-                    color: MyColors.mainColor,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold),
-              ),
+            child: Column(
+              children: [
+                FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      'Bienvenido a Consult IT',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: Fonts.secondaryFont,
+                          color: MyColors.mainColor,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 25),
+                  child: Text(
+                    'Decide como deseas registrarte a Consult IT: Emprendedor o Asesor financiero.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: Fonts.secondaryFont,
+                      color: MyColors.mainColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           // Tabs para emprendedor y asesores.
@@ -106,13 +122,7 @@ class _RegistrationPageState extends State<RegistrationPage>
                 visible: selectedIndex == 0,
               ),
               Visibility(
-                child: Container(
-                  height: 1000,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text('Content right'),
-                  ),
-                ),
+                child: buildAsesorTab(),
                 maintainState: true,
                 visible: selectedIndex == 1,
               ),
@@ -291,5 +301,177 @@ class _RegistrationPageState extends State<RegistrationPage>
     );
   }
 
-  Widget buildAsesorTab() {}
+  Widget buildAsesorTab() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: Column(
+        children: [
+          // Inicio de contenedor para datos personales
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                      fit: FlexFit.loose,
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(Icons.supervised_user_circle,
+                            size: 28, color: Color.fromRGBO(3, 90, 166, 1)),
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          'DATOS PERSONALES',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: Fonts.primaryFont,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // Campos para ingresar informacion personal
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, .2),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(
+                    children: [
+                      // Nombre segun documento de identidad
+                      inputField(
+                          hintText: 'Ingrese su nombre',
+                          labelText: 'Nombre completo',
+                          icon: Icons.person_outline,
+                          uppercase: false,
+                          bold: false),
+                      // Correo Electronico
+                      inputField(
+                          hintText: 'Ingrese su correo',
+                          labelText: 'Correo electrónico',
+                          icon: Icons.email,
+                          uppercase: false,
+                          bold: false),
+                      // Telefono de contacto
+                      inputField(
+                          hintText: 'Ingrese su telefono de contacto',
+                          labelText: 'Telefono de contacto',
+                          icon: Icons.phone,
+                          uppercase: false,
+                          bold: false),
+                      // Telefono de contacto
+                      inputField(
+                          hintText: 'Ej. Licenciado, Ingeniero, etc',
+                          labelText: 'Titulo profesional',
+                          icon: Icons.verified_user,
+                          uppercase: false,
+                          bold: false),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Fin de datos personales
+
+          // Inicio de contenedor para datos de la aplicacion
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                      fit: FlexFit.loose,
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(Icons.person_add,
+                            size: 28, color: Color.fromRGBO(3, 90, 166, 1)),
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      flex: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          'REGISTRO DE USUARIO',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: Fonts.primaryFont,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // Campos para ingresar informacion personal
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, .2),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: Column(
+                    children: [
+                      // Nombre segun documento de identidad
+                      inputField(
+                          hintText: 'Ingrese su usuario',
+                          labelText: 'Nombre de usuario',
+                          icon: Icons.person_outline,
+                          uppercase: false,
+                          bold: false),
+                      // Correo Electronico
+                      inputField(
+                          hintText: 'Ingrese su contraseña',
+                          labelText: 'Contraseña',
+                          icon: Icons.lock,
+                          uppercase: false,
+                          bold: false,
+                          obscure: true),
+                      // Telefono de contacto
+                      inputField(
+                          hintText: 'Repita su contraseña',
+                          labelText: 'Repetir contraseña',
+                          icon: Icons.lock,
+                          uppercase: false,
+                          bold: false,
+                          obscure: true),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Fin de datos personales
+        ],
+      ),
+    );
+  }
 }
