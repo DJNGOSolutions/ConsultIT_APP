a1153892fa184ed10b232af34fd385526a2ec03e
Ronald Vega <ronald.vega48@gmail.com>
2020-10-26 18:42:11 -0600

    Appbar modifications

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:consult_it_app/utils/styles.dart';
-import 'package:consult_it_app/utils/router.dart';
 import 'package:flutter/material.dart';
 import '../utils/widgets_lib.dart' as myWidgets;
 
@@ -8,40 +7,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(
-              Icons.menu,
-              color: MyColors.mainColor,
-            ),
-            onPressed: () => Navigator.pushNamed(context, LOGIN_ROUTE)),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 4.0),
-              child: Text(
-                "CONSULT",
-                style: Styles.headerTextStyle.apply(color: MyColors.mainColor),
-              ),
-            ),
-            Text(
-              "IT",
-              style: Styles.headerTextStyle.apply(color: MyColors.accentColor),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.11,
-            )
-          ],
-        ),
-      ),
+      appBar: myWidgets.customAppBar(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [_buildMyBusinessSection(context)],
+        children: [
+          _buildMyBusinessSection(context),
+          _buildMoreOptionsSection(context)
+        ],
       ),
       floatingActionButton: myWidgets.faqbot(context),
     );
@@ -65,7 +38,7 @@ class HomePage extends StatelessWidget {
                             image: AssetImage(
                                 'assets/images/icons/ProjectManagement.png'))),
                   ),
-                  flex: 4,
+                  flex: 1,
                   fit: FlexFit.loose,
                 ),
                 Flexible(
@@ -75,7 +48,78 @@ class HomePage extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.symmetric(horizontal: 20),
                         child: Text(
-                          'Mis Proyectos',
+                          'Mis Comercios',
+                          style: Styles.headerTextStyle,
+                        ),
+                      ),
+                      Tooltip(
+                        message: "Ver todos mis comercios",
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: MyColors.mainColor,
+                            ),
+                            onPressed: null),
+                      )
+                    ],
+                  ),
+                  flex: 7,
+                  fit: FlexFit.tight,
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15.0, left: 15.0),
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, bottom: 5.0),
+                  child: myWidgets.businessWidget(
+                      businessName: "Venta de textiles s.a. de c.v."),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: myWidgets.businessWidget(
+                      businessName: "Tienda de ropa online",
+                      businessImage: "assets/images/icons/OnlineShoping.png"),
+                ),
+                myWidgets.addBusinessWidget()
+              ],
+            ),
+          )
+        ],
+      );
+
+  _buildMoreOptionsSection(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(top: 15.0, right: 0.0, left: 20.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Icon(
+                    Icons.more_vert,
+                    size: 35.0,
+                    color: MyColors.mainColor,
+                  ),
+                  flex: 1,
+                  fit: FlexFit.loose,
+                ),
+                Flexible(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          'Más Acciones',
                           style: Styles.headerTextStyle,
                         ),
                       ),
@@ -93,28 +137,6 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(top: 15.0, left: 15.0),
-            height: 200,
-            width: MediaQuery.of(context).size.width,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: myWidgets.businessWidget(
-                      businessName: "Venta de textiles s.a. de c.v."),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: myWidgets.businessWidget(
-                      businessName: "Tienda de ropa online",
-                      businessImage: "assets/images/icons/OnlineShoping.png"),
-                ),
-                myWidgets.addBusinessWidget()
-              ],
-            ),
-          )
         ],
       );
 }
--- lib/utils/widgets_lib.dart
+++ lib/utils/widgets_lib.dart
@@ -163,41 +163,45 @@ customButton(
     );
 
 Widget addBusinessWidget() {
-  return NeuCard(
-    bevel: 24,
-    curveType: CurveType.emboss,
-    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-    margin: EdgeInsets.only(top: 10.0, right: 15.0),
-    decoration: NeumorphicDecoration(
-        borderRadius: BorderRadius.circular(12.0), color: Colors.white),
-    width: 190,
-    height: 140,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Flexible(
-          fit: FlexFit.loose,
-          flex: 6,
-          child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/icons/Unsubscribed.png'))),
-          ),
-        ),
-        Flexible(
-          fit: FlexFit.loose,
-          flex: 3,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
-            child: Text(
-              'Agregar Nueva Empresa'.toUpperCase(),
-              style: Styles.bodyTextStyle.apply(color: Colors.black54),
-              textAlign: TextAlign.center,
+  return Tooltip(
+    message: "Agregar un nuevo comercio",
+    child: NeuCard(
+      bevel: 24,
+      curveType: CurveType.emboss,
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+      margin: EdgeInsets.only(top: 10.0, right: 15.0),
+      decoration: NeumorphicDecoration(
+          borderRadius: BorderRadius.circular(12.0), color: Colors.white),
+      width: 190,
+      height: 140,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            fit: FlexFit.loose,
+            flex: 6,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/icons/Unsubscribed.png'))),
             ),
           ),
-        ),
-      ],
+          Flexible(
+            fit: FlexFit.loose,
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+              child: Text(
+                'Agregar nuevo comercio'.toUpperCase(),
+                style: Styles.bodyTextStyle.apply(color: Colors.black54),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
@@ -205,40 +209,106 @@ Widget addBusinessWidget() {
 Widget businessWidget(
     {@required String businessName,
     String businessImage = 'assets/images/icons/FolderDataColor.png'}) {
-  return NeuCard(
-      bevel: 24,
-      curveType: CurveType.emboss,
-      padding: EdgeInsets.all(8),
-      margin: EdgeInsets.only(top: 10.0, right: 15.0),
-      decoration: NeumorphicDecoration(
-          borderRadius: BorderRadius.circular(12.0), color: Colors.white),
-      width: 190,
-      height: 140,
-      child: Column(
+  return Tooltip(
+    message: "Ver detalle de: ${businessName.toUpperCase()}",
+    child: NeuCard(
+        bevel: 24,
+        curveType: CurveType.emboss,
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.only(top: 10.0, right: 15.0),
+        decoration: NeumorphicDecoration(
+            borderRadius: BorderRadius.circular(12.0), color: Colors.white),
+        width: 190,
+        height: 140,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(businessImage))),
+              ),
+              fit: FlexFit.loose,
+              flex: 3,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Flexible(
+              child: Text(
+                businessName.toUpperCase(),
+                style: Styles.bodyTextStyle.apply(color: Colors.black54),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
+              fit: FlexFit.loose,
+              flex: 4,
+            ),
+          ],
+        )),
+  );
+}
+
+Widget customAppBar(BuildContext context) => AppBar(
+      bottomOpacity: 0,
+      toolbarOpacity: 1.0,
+      leading: Padding(
+        padding:
+            const EdgeInsets.only(left: 8.0, top: 4.0, right: 4.0, bottom: 4.0),
+        child: Tooltip(
+          message: "Menú",
+          child: NeuCard(
+            bevel: 16,
+            curveType: CurveType.flat,
+            decoration: NeumorphicDecoration(
+                borderRadius: BorderRadius.circular(12.0), color: Colors.white),
+            child: Icon(
+              Icons.menu,
+              color: MyColors.mainColor,
+            ),
+          ),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(
+              left: 4.0, top: 4.0, right: 8.0, bottom: 4.0),
+          child: Tooltip(
+            message: "Cerrar Sesión",
+            child: NeuButton(
+              onPressed: () => Navigator.pushNamed(context, LOGIN_ROUTE),
+              decoration: NeumorphicDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  color: Colors.white),
+              child: Container(
+                height: 25,
+                width: 25,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/icons/LogOut.png'))),
+              ),
+            ),
+          ),
+        ),
+      ],
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      title: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Flexible(
-            child: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(businessImage))),
-            ),
-            fit: FlexFit.loose,
-            flex: 3,
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Flexible(
+          Padding(
+            padding: const EdgeInsets.only(right: 4.0),
             child: Text(
-              businessName.toUpperCase(),
-              style: Styles.bodyTextStyle.apply(color: Colors.black54),
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
+              "CONSULT",
+              style: Styles.headerTextStyle.apply(color: MyColors.mainColor),
             ),
-            fit: FlexFit.loose,
-            flex: 4,
+          ),
+          Text(
+            "IT",
+            style: Styles.headerTextStyle.apply(color: MyColors.accentColor),
           ),
         ],
-      ));
-}
+      ),
+    );
